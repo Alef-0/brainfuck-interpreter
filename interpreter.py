ad80d31fd4214f330cb8230d848652c4c97ab92f
Alef-0 <AlefGabryel@protonmail.com>
2021-06-03 06:36:43 -0300

    Final Change
    Last Renaming

--- interpreter.py
+++ interpreter.py
@@ -2,51 +2,51 @@ from sys import argv
 from readchar import readchar as rchar
 import numpy
 
-tape = numpy.zeros(30000,dtype = numpy.uint8)
+memory = numpy.zeros(30000,dtype = numpy.uint8)
 pointer : int = 0
-program : str
+code : str
 quantity : int
 
 def interpreter():
-    global pointer, tape, program, quantity
+    global pointer, memory, code, quantity
     brackets = 0
     flag = True
 
     #for factor
     letter = 0
-    quantity = len(program)
+    quantity = len(code)
 
     while flag:
-        if program[letter] == '+': tape[pointer] += 1
-        elif program[letter] == '-': tape[pointer] -= 1
-        elif program[letter] == '>': pointer += 1
-        elif program[letter] == '<': pointer -= 1
-        elif program[letter] == '.': print(chr(tape[pointer]),end='')
-        elif program[letter] == ',': tape[pointer] = ord(input())
+        if code[letter] == '+': memory[pointer] += 1
+        elif code[letter] == '-': memory[pointer] -= 1
+        elif code[letter] == '>': pointer += 1
+        elif code[letter] == '<': pointer -= 1
+        elif code[letter] == '.': print(chr(memory[pointer]),end='')
+        elif code[letter] == ',': memory[pointer] = ord(input())
         #Loops
-        elif program[letter] == '[':
-            if not tape[pointer]:
+        elif code[letter] == '[':
+            if not memory[pointer]:
                 brackets += 1
-                while program[letter] != ']' or brackets != 0:
+                while code[letter] != ']' or brackets != 0:
                     letter += 1
-                    if program[letter] == '[': brackets += 1
-                    elif program[letter] == ']': brackets -=1
-        elif program[letter] == ']':
-            if tape[pointer]:
+                    if code[letter] == '[': brackets += 1
+                    elif code[letter] == ']': brackets -=1
+        elif code[letter] == ']':
+            if memory[pointer]:
                 brackets += 1
-                while program[letter] != '[' or brackets != 0:
+                while code[letter] != '[' or brackets != 0:
                     letter -= 1
-                    if program[letter] == ']': brackets += 1
-                    elif program[letter] == '[': brackets -=1
+                    if code[letter] == ']': brackets += 1
+                    elif code[letter] == '[': brackets -=1
         
         #Exit Cases
         letter += 1
         if letter > quantity: flag = False
 
 def validates():
-    global program
+    global code
     counting = 0
-    for letters in program:
+    for letters in code:
         if letters == '[': counting += 1
         elif letters == ']':
             if not counting: return False
@@ -54,12 +54,12 @@ def validates():
     return counting == 0
         
 def main(file_name = 'file.b'):
-    global pointer, tape, program, quantity
-    with open(file_name,'r') as file: program = file.read()
-    quantity = len(program)-1
+    global pointer, memory, code, quantity
+    with open(file_name,'r') as file: code = file.read()
+    quantity = len(code)-1
     if validates(): interpreter()
     else: print("There's something wrong with the code. Probably with the ([]) operators.")
-    print(f'\nMemory: {tape}')
+    print(f'\nMemory: {memory}')
 
 if __name__ == '__main__':
     if argv[-1].endswith('.b'): main(argv[-1])
